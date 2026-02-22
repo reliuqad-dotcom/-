30983529b4e4a7a8c7132d1bcdfd271157dd3772
reliuqad-dotcom <reliuqad@gmail.com>
2026-02-22 15:28:00 +0900

    Add files via upload

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
+# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./stock_simulator.db"
+# 괄호 [ ] 빼고 비밀번호만 넣으세요!
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.iubmxvbgansukndtsep:rltgjqmEoansdpaksemsqlqjs!!2@@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
